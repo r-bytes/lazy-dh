3d9a5fffa78cbcec92b756f0267001717d8b6a22
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-05-12 18:53:48 +0200

    feat: logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-background">
       <Header />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href=""
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -17,7 +17,7 @@ export default function Home() {
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">Find in-depth information about Next.js features and API.</p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ModeToggle } from "../ui/toggle-mode";
 
 type NavigationItem = {
@@ -6,7 +7,7 @@ type NavigationItem = {
 };
 
 type Props = {
-  navigationList: NavigationItem[];
+  navigationList?: NavigationItem[];
 };
 
 const NAVIGATION_LIST: NavigationItem[] = [
@@ -19,11 +20,20 @@ const NAVIGATION_LIST: NavigationItem[] = [
 const Header = (props: Props) => {
   return (
     <div className="z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex">
-      <div className="border p-12 w-96"> LOGO </div>
-      <div className="border w-full">
-        <ul className="flex space-x-12 border justify-end py-12">
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:invert"
+          src="/logo.svg"
+          alt="Next.js Logo"
+          width={300}
+          height={20}
+          priority
+        />
+      </div>
+      <div className="w-full">
+        <ul className="flex space-x-12 justify-end py-12">
           {NAVIGATION_LIST.map((item, index, arr) => (
-            <li key={item.order} className={`hover:cursor-pointer font-bold tracking-wide ${index === arr.length - 1 ? "pr-8" : ""}`}>
+            <li key={item.order} className={`hover:cursor-pointer hover:text-primary font-bold tracking-wide ${index === arr.length - 1 ? "pr-8" : ""}`}>
               {item.title}
             </li>
           ))}
