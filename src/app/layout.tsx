b273d49275122c39d1e39c13f49ac9dfb3923dc6
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-06-02 20:15:30 +0200

    feat: cart context, toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import Header from "@/components/navigation/header";
-import MobileNavbar from "@/components/navigation/mobileNavbar";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { CartProvider } from "@/context/StateContext";
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -22,21 +23,24 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${montserrat.className} flex min-h-screen flex-col`}>
         {/* Ensure body takes up full height */}
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Header />
-          {/* mobile nav */}
-          {/* <main className="flex-grow overflow-auto"> */}
-          <main>
+        <CartProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <Toaster />
+            <Header />
             {/* mobile nav */}
-            {/* Make main content scrollable */}
-            {children}
-          </main>
-          {/* mobile nav */}
-          {/* <MobileNavbar cn={`fixed bottom-0 h-16 bg-black w-full sm:hidden sm:h-auto`} />{" "} */}
-          
-          {/* mobile nav */}
-          {/* Stick to bottom on small screens, otherwise behave normally */}
-        </ThemeProvider>
+            {/* <main className="flex-grow overflow-auto"> */}
+            <main>
+              {/* mobile nav */}
+              {/* Make main content scrollable */}
+              {children}
+            </main>
+            {/* mobile nav */}
+            {/* <MobileNavbar cn={`fixed bottom-0 h-16 bg-black w-full sm:hidden sm:h-auto`} />{" "} */}
+
+            {/* mobile nav */}
+            {/* Stick to bottom on small screens, otherwise behave normally */}
+          </ThemeProvider>
+        </CartProvider>
       </body>
     </html>
   );
---
+++ src/context/StateContext.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+import { toast } from "react-hot-toast";
+
+type Product = {
+  _id: number;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  quantity: number;
+  slug: string;
+};
+
+type ContextProps = {
+  showCart: boolean;
+  cartItems: Product[];
+  totalPrice: number;
+  totalQuantities: number;
+  qty: number;
+  incQty: () => void;
+  decQty: () => void;
+  onAdd: (product: Product, quantity: number) => void;
+  setShowCart: React.Dispatch<React.SetStateAction<boolean>>;
+  toggleCartItemQuantity: (id: number | string, value: "inc" | "dec") => void;
+  onRemove: (product: Product) => void;
+  setCartItems: (value: React.SetStateAction<Product[]>) => void;
+  setTotalQuantities: (value: React.SetStateAction<number>) => void;
+  setTotalPrice: (value: React.SetStateAction<number>) => void;
+};
+
+export const CartContext = createContext<ContextProps>({
+  showCart: false,
+  cartItems: [],
+  totalPrice: 0,
+  totalQuantities: 0,
+  qty: 0,
+  incQty: () => {},
+  decQty: () => {},
+  onAdd: () => {},
+  setShowCart: () => {},
+  toggleCartItemQuantity: () => {},
+  onRemove: () => {},
+  setCartItems: () => {},
+  setTotalQuantities: () => {},
+  setTotalPrice: () => {},
+});
+
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [showCart, setShowCart] = useState<boolean>(false);
+  const [cartItems, setCartItems] = useState<Product[]>([
+    {
+      _id: 1,
+      name: "Ouzo Paralia",
+      description:
+        "Ouzo Paralia is a premium anise-flavored Greek spirit, perfect for leisurely moments by the sea. Embrace the essence of Greek summer with every sip of Ouzo Paralia.",
+      price: 15.0,
+      image: "ouzo-paralia.png",
+      slug: "ouzo-paralia",
+      quantity: 1000,
+    },
+    {
+      _id: 2,
+      name: "Ouzo Paralia",
+      description:
+        "Ouzo Paralia is a premium anise-flavored Greek spirit, perfect for leisurely moments by the sea. Embrace the essence of Greek summer with every sip of Ouzo Paralia.",
+      price: 15.0,
+      image: "ouzo-paralia.png",
+      slug: "ouzo-paralia",
+      quantity: 1000,
+    },
+  ]);
+
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const [totalQuantities, setTotalQuantities] = useState<number>(0);
+  const [qty, setQty] = useState<number>(1);
+
+  let foundProduct: Product | undefined;
+  let index: number;
+
+  const onAdd = (product: Product, quantity: number) => {
+    const checkProductInCard = cartItems.find((item) => item._id === product._id);
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCard) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity, // Ensure updated quantity is included
+          };
+        }
+        return cartProduct; // Explicitly return the original cartProduct if no update is needed
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      const newProduct = { ...product, quantity }; // Create a copy of the product with updated quantity
+      setCartItems([...cartItems, newProduct]); // Add the new product to the cart
+    }
+
+    toast.success(`${quantity} ${product.name} added to the cart.`);
+  };
+
+  const onRemove = (product: Product) => {
+    foundProduct = cartItems.find((item: Product) => item._id === product._id);
+    const newCartItems = cartItems.filter((item: Product) => item._id !== product._id);
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct!.price * foundProduct!.quantity);
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct!.quantity);
+    setCartItems(newCartItems);
+  };
+
+  const toggleCartItemQuantity = (id: number | string, value: "inc" | "dec") => {
+    foundProduct = cartItems.find((item) => item._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+
+    const newCartItems = cartItems.filter((item) => item._id !== id);
+
+    if (value === "inc" && foundProduct) {
+      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct!.price);
+      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+    } else if (value === "dec" && foundProduct) {
+      if (foundProduct.quantity > 1) {
+        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct!.price);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+      }
+    }
+  };
+
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+      return prevQty - 1;
+    });
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+        setShowCart,
+        toggleCartItemQuantity,
+        onRemove,
+        setCartItems,
+        setTotalQuantities,
+        setTotalPrice,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCartContext = () => useContext(CartContext);
