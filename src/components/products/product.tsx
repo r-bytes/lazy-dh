19d01baf95fd83a71b80882aec4ce683653b0945
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-06-02 20:14:59 +0200

    product page

--- src/components/products/product.tsx
+++ src/components/products/product.tsx
@@ -1,67 +1,117 @@
 "use client";
 
 import { Product as ProductType } from "@/lib/definitions";
-import { Heart } from "lucide-react";
+import { Heart, Minus, Plus, ShoppingCart } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardDescription, CardTitle } from "../ui/card";
 
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { useCartContext } from "@/context/StateContext";
+import Image from "next/image";
 
-const Product = ({ product: { title, price, image, description, slug } }: { product: ProductType }) => {
+const Product = ({ product }: { product: ProductType }) => {
+  // Hooks
   const pathname = usePathname();
   const router = useRouter();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useCartContext();  
+
+  // States
+  const [isHoveredOn, setIsHoveredOn] = useState<boolean>(false);
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+
+  // Variables
   const backgroundImageStyle = {
-    backgroundImage: `url('/${image}')`,
+    backgroundImage: `url('/${product.image}')`,
     backgroundSize: "80%",
     backgroundPosition: "50% 30%",
   };
 
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const PRICE_TOTAL = 200;
 
-  const [isFavorite, setIsFavorite] = useState<boolean>(false);
-
+  // Functions
   const handleToggleFavorite = () => {
     setIsFavorite(!isFavorite);
   };
 
-  const handleOpenDialog = () => {
-    setIsOpen(true);
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+    setShowCart(true);
   };
 
   return (
-    <>
-      {/* <Dialog isOpen={isOpen} onClose={() => setIsOpen(false)}> */}
-      <Dialog>
-        <DialogTrigger asChild>
-          <Card
-            onClick={handleOpenDialog}
-            className="relative z-10 flex h-[32rem] w-72 flex-col rounded-2xl bg-neutral-300/10 bg-no-repeat dark:bg-neutral-800/30"
-            style={backgroundImageStyle}
+    <Dialog>
+      {/* Conditionally add asChild based on isHoveredOn state */}
+      <DialogTrigger {...(!isHoveredOn ? { asChild: true } : {})}>
+        <Card
+          onClick={(e) => e.stopPropagation()} // Prevent the dialog from opening immediately upon clicking the card
+          className="relative z-10 flex h-[32rem] w-80 flex-col rounded-2xl bg-neutral-300/10 bg-no-repeat hover:cursor-pointer dark:bg-neutral-800/30 md:w-60"
+          style={backgroundImageStyle}
+        >
+          <Button
+            onClick={handleToggleFavorite}
+            onMouseEnter={() => setIsHoveredOn(true)}
+            onMouseLeave={() => setIsHoveredOn(false)}
+            className="ml-[75%] mt-4 h-12 w-12 rounded-full bg-muted-foreground/10 hover:bg-primary/70"
           >
-            <Button
-              onClick={handleToggleFavorite}
-              className="absolute right-4 top-4 z-50 h-12 w-12 rounded-full bg-muted-foreground/10 hover:bg-primary/70"
-            >
-              <Heart color={isFavorite ? "red" : ""} fill={isFavorite ? "red" : "bg-muted-foreground/30"} />
-            </Button>
-            <CardContent className="flex flex-1 flex-col items-center justify-end rounded-2xl">
-              <div className="mb-12 w-full text-center">
-                <CardTitle className="mb-2 flex-1 text-lg">{title}</CardTitle>
-                <CardDescription className="flex-1 text-2xl font-bold"> €{price},-</CardDescription>
-              </div>
-            </CardContent>
-          </Card>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>{title}</DialogTitle>
-            <DialogDescription>{description}</DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </>
+            <Heart color={isFavorite ? "red" : ""} fill={isFavorite ? "red" : "bg-muted-foreground/30"} />
+          </Button>
+          <CardContent className="flex flex-1 flex-col items-center justify-end rounded-2xl">
+            <div className="mb-12 w-full text-center">
+              <CardTitle className="mb-2 flex-1 text-lg">{product.name}</CardTitle>
+              <CardDescription className="flex-1 text-2xl font-bold"> €{product.price},-</CardDescription>
+            </div>
+          </CardContent>
+        </Card>
+      </DialogTrigger>
+      <DialogContent className="flex flex-col bg-zinc-100 dark:bg-zinc-900">
+        {/* Top */}
+        <Image className="mx-auto my-8" src={`/${product.image}`} alt={product.name} width={300} height={300} />
+        {/* Middle */}
+        <DialogHeader className="mx-[-24px] mb-8 rounded-t-3xl bg-zinc-200/50 p-12 dark:bg-zinc-200">
+          <DialogTitle className="mb-4 text-center text-2xl dark:text-black">{product.name}</DialogTitle>
+          <DialogDescription className="mx-8 mb-4 text-center leading-relaxed tracking-wider dark:text-black">
+            {product.description}
+          </DialogDescription>
+        </DialogHeader>
+        {/* Bottom */}
+        <DialogFooter className="mx-[-24px] mb-[-25px] mt-[-60px] flex h-32 flex-col items-center justify-between rounded-b-lg rounded-t-2xl bg-zinc-100 px-8 dark:bg-zinc-900 md:px-0">
+          <div id="priceTotal" className="flex-1 text-4xl tracking-wide sm:ml-3 md:ml-12">
+            <span className="mr-1 text-xs font-bold dark:text-white">€</span>
+            <span className="font-bold tracking-wide dark:text-white">{PRICE_TOTAL}</span>
+          </div>
+          <div className="flex flex-col items-center space-y-4 p-4 pr-6">
+            <div className="quantity flex h-8 w-full items-center justify-center ">
+              <h3> Aantal: </h3>
+              <p className="quantity-desc ml-12 flex flex-1 justify-center">
+                <span
+                  className="minus flex w-12 items-center justify-center border border-muted-foreground/40 text-center text-red-700"
+                  onClick={decQty}
+                >
+                  <Minus />
+                </span>
+                <span className="num flex w-8 items-center justify-center border border-muted-foreground/40 text-center "> {qty} </span>
+                <span
+                  className="plus flex w-12 items-center justify-center border border-muted-foreground/40 text-center text-green-700"
+                  onClick={incQty}
+                >
+                  <Plus />
+                </span>
+              </p>
+            </div>
+            <div className="buttons flex gap-3 w-full items-center justify-end">
+              <Button type="button" className="addToCart flex gap-2" onClick={() => onAdd(product, qty)}>
+                Voeg Toe
+              </Button>
+              <Button type="button" className="buy-now" onClick={handleBuyNow}>
+                <ShoppingCart />
+              </Button>
+            </div>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
