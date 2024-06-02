8b2232c9c419cba2d3b3d7d7f1d290fa3bd1bcd2
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-06-02 17:54:49 +0200

    content on home, mobile hamburger menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,79 @@
+"use client";
+
+import ProductList from "@/components/products/product-list";
+import { Button } from "@/components/ui/button";
+import MaxWidthWrapper from "@/components/ui/max-width-wrapper";
+import Title from "@/components/ui/title";
+import { Product as ProductType } from "@/lib/definitions";
+import { useRouter } from "next/navigation";
+
+const AANBIEDINGEN: ProductType[] = [
+  {
+    _id: 1,
+    name: "Ouzo Paralia",
+    description:
+      "Ouzo Paralia is a premium anise-flavored Greek spirit, perfect for leisurely moments by the sea. Embrace the essence of Greek summer with every sip of Ouzo Paralia.",
+    price: 15.0,
+    image: "ouzo-paralia.png",
+    slug: "ouzo-paralia",
+    quantity: 1000,
+  },
+  {
+    _id: 2,
+    name: "Ouzo Paralia",
+    description:
+      "Ouzo Paralia is a premium anise-flavored Greek spirit, perfect for leisurely moments by the sea. Embrace the essence of Greek summer with every sip of Ouzo Paralia.",
+    price: 15.0,
+    image: "ouzo-paralia.png",
+    slug: "ouzo-paralia",
+    quantity: 1000,
+  },
+  {
+    _id: 3,
+    name: "Ouzo Paralia",
+    description:
+      "Ouzo Paralia is a premium anise-flavored Greek spirit, perfect for leisurely moments by the sea. Embrace the essence of Greek summer with every sip of Ouzo Paralia.",
+    price: 15.0,
+    image: "ouzo-paralia.png",
+    slug: "ouzo-paralia",
+    quantity: 1000,
+  },
+  {
+    _id: 4,
+    name: "Ouzo Paralia",
+    description:
+      "Ouzo Paralia is a premium anise-flavored Greek spirit, perfect for leisurely moments by the sea. Embrace the essence of Greek summer with every sip of Ouzo Paralia.",
+    price: 15.0,
+    image: "ouzo-paralia.png",
+    slug: "ouzo-paralia",
+    quantity: 1000,
+  },
+  {
+    _id: 4,
+    name: "Ouzo Paralia",
+    description:
+      "Ouzo Paralia is a premium anise-flavored Greek spirit, perfect for leisurely moments by the sea. Embrace the essence of Greek summer with every sip of Ouzo Paralia.",
+    price: 15.0,
+    image: "ouzo-paralia.png",
+    slug: "ouzo-paralia",
+    quantity: 1000,
+  },
+];
 
 export default function Home() {
+  const navigate = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-background p-24">
-      {/* <section> */}
-        {/* <MaxWidthWrapper>
-          
-        </MaxWidthWrapper> */}
-      {/* </section> */}
+    <main className="flex min-h-screen flex-col items-center justify-between bg-background py-24">
+      <section id="acties">
+        <Title name={"Aanbiedingen"} />
+        <MaxWidthWrapper>
+          <ProductList products={AANBIEDINGEN.slice(0, 4)} />
+        </MaxWidthWrapper>
+        <div className="mx-auto flex justify-center my-8">
+          <Button onClick={() => navigate.push("/assortiment/aanbiedingen")}>View More</Button>
+        </div>
+      </section>
     </main>
   );
 }
--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,27 +1,32 @@
-"use client"
+"use client";
 
+import { MenuIcon } from "lucide-react";
 import { Montserrat, Roboto } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-import { ModeToggle } from "../ui/toggle-mode";
-import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { ModeToggle } from "../ui/toggle-mode";
 
+// Fonts
 const montserrat = Montserrat({ subsets: ["latin"] });
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "500", "700"],
 });
 
+// Types
 type NavigationItem = {
   order: number;
   title: string;
 };
 
+// Props
 type Props = {
   navigationList?: NavigationItem[];
 };
 
+// Variables
 const NAVIGATION_LIST: NavigationItem[] = [
   { title: "Assortiment", order: 0 },
   { title: "Account", order: 1 },
@@ -30,14 +35,44 @@ const NAVIGATION_LIST: NavigationItem[] = [
 ];
 
 const Header = (props: Props) => {
-  const [currentPath, setCurrentPath] = useState()
-  const pathName = usePathname()
-  
-  return (
-    <div className="z-10 mx-auto hidden w-full max-w-7xl items-center justify-between font-mono text-sm sm:flex sm:flex-col lg:flex-row">
-      <Image className="dark:invert" src="/logo.svg" alt="Next.js Logo" width={300} height={20} priority />
+  // Hooks
+  const pathName = usePathname();
 
-      <div className="flex items-center justify-center sm:w-full sm:max-w-2xl mx-auto lg:mr-10">
+  // States
+  const [currentPath, setCurrentPath] = useState();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="z-10 mx-auto w-full max-w-7xl items-center justify-between font-mono text-sm sm:flex sm:flex-col lg:flex-row">
+      <div className="flex justify-between items-center mx-8 sm:mx-16 mt-12 w-full">
+        {/* Logo */}
+        <Link href={"/"}>
+          <Image className="mx-auto dark:invert" src="/logo.svg" alt="Lazy Den Haag Logo" width={300} height={20} priority />
+        </Link>
+
+        {/* Hamburger Menu for Small Screens */}
+        <button className="flex w-full items-end justify-end pr-4 sm:pr-8 lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          <MenuIcon size={24} /> {/* Use MenuIcon from Lucid */}
+        </button>
+      </div>
+      {/* Mobile Navigation Menu */}
+      <div className={`lg:hidden ${menuOpen ? "block" : "hidden"}`}>
+        <ul className="flex flex-col items-start space-y-4 p-4">
+          {NAVIGATION_LIST.map((item, index, arr) => (
+            <li
+              key={item.order}
+              className={`font-semibold tracking-wide hover:cursor-pointer hover:text-primary ${index === arr.length - 2 ? "rounded-md bg-primary px-3 py-2 text-muted-foreground hover:bg-primary-foreground/30 hover:text-muted-foreground dark:text-background dark:hover:text-primary" : currentPath === item.title.toLocaleLowerCase().replace("/", "") ? "text-secondary" : ""}`}
+            >
+              <Link className={roboto.className} href={`/${item.title.toLowerCase() === "acties" ? "promoties" : item.title.toLowerCase()}`}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Desktop Navigation */}
+      <div className="mx-auto hidden items-center justify-center sm:w-full sm:max-w-2xl lg:mr-10 lg:flex">
         <div className="w-full">
           <ul className="mr-8 flex items-baseline justify-end space-x-6 py-12">
             {NAVIGATION_LIST.map((item, index, arr) => (
@@ -45,10 +80,7 @@ const Header = (props: Props) => {
                 key={item.order}
                 className={`font-semibold tracking-wide hover:cursor-pointer hover:text-primary ${index === arr.length - 2 ? "mr-8 rounded-md bg-primary px-3 py-2 text-muted-foreground hover:bg-primary-foreground/30 hover:text-muted-foreground dark:text-background dark:hover:text-primary" : currentPath === item.title.toLocaleLowerCase().replace("/", "") ? "text-secondary" : ""}`}
               >
-                <Link
-                  className={roboto.className}
-                  href={`/${item.title.toLowerCase() === "acties" ? "promoties" : item.title.toLowerCase()}`}
-                >
+                <Link className={roboto.className} href={`/${item.title.toLowerCase() === "acties" ? "promoties" : item.title.toLowerCase()}`}>
                   {item.title}
                 </Link>
               </li>
