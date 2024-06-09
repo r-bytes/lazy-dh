fd7fd34d9ae76b47eb51d3312ed4320b5ac261fc
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-06-09 12:39:27 +0200

    .

--- sanity/lazo-dh/sanity.config.ts
+++ sanity/lazo-dh/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'lazo-dh',
 
-  projectId: 'rx2p8wni',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
   plugins: [deskTool(), visionTool()],
 
