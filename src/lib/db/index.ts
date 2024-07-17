be8d3b3caaf395ceb39e536c3d92ad7d3a4b265e
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-07-18 00:14:22 +0200

    switch to supabase

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,7 +3,10 @@ import postgres from "postgres";
 
 import * as schema from "./schema";
 
-const pool = postgres(process.env.AUTH_DRIZZLE_URL!, { max: 1 });
+const pool = postgres(process.env.AUTH_DRIZZLE_URL!, {
+  max: 1,
+  ssl: { rejectUnauthorized: false }, // SSL configuration for secure connection
+});
 
 export const db = drizzle(pool, {
   schema,
