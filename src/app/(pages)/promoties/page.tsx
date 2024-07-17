5c074946624bb504b303992edbf45c8cd451ed60
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-07-17 22:13:20 +0200

    fix: build error

--- src/app/(pages)/promoties/page.tsx
+++ src/app/(pages)/promoties/page.tsx
@@ -6,23 +6,21 @@ import Title from "@/components/ui/title";
 import { fetchProducts } from "@/lib/sanity/fetchProducts";
 import Product from "@/lib/types/product";
 
-export default async function Promotions({ products }: { products: Product[] }) {
+export default async function Promotions() {
   // Check if products array is empty
-  const isEmpty = !products;
   const fetchedProducts = await fetchProducts("?type=aanbiedingen");
+  const isEmpty = !fetchedProducts;
 
   return (
     <div className="flex flex-col items-center justify-center">
       <Title name={"Aanbiedingen"} cn="py-4" />
       <MaxWidthWrapper>
         {!isEmpty ? (
-          <ProductList products={products} />
-        ) : isEmpty && fetchedProducts ? (
           <ProductList products={fetchedProducts} />
         ) : (
           // Fallback UI when products doesnt exist
           <div className="py-5 text-center">
-            <p>No promotions available.</p>
+            <p> Geen producten in de aanbieding. </p>
           </div>
         )}
       </MaxWidthWrapper>
