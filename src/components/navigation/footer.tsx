95a2c510eb6f30befc09e7754ff0710f4a325a8b
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-07-23 21:56:07 +0200

    fixes

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -6,7 +6,7 @@ type FooterProps = {
 
 const Footer: React.FC<FooterProps> = ({ year = new Date().getFullYear() }) => {
   return (
-    <footer className="bg-zinc-900 p-4 text-center text-white mt-44">
+    <footer className="bg-zinc-900/20 p-4 text-center text-white mt-44">
       <div className="container mx-auto px-4">
         <p className="text-sm sm:text-base">&copy; {year} Lazo Den Haag. All rights reserved.</p>
         <div className="mt-2 flex flex-col justify-center space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
--- src/components/products/product-carousel.tsx
+++ src/components/products/product-carousel.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { Product as ProductType } from "@/lib/types/product";
@@ -5,22 +6,29 @@ import Product from "./product";
 
 export function CarouselSpacing({ products, cn }: { products: ProductType[] | null; cn?: string }) {
   return (
-    <Carousel className="max-w-96 sm:max-w-full mt-12">
-      <CarouselContent>
+    <Carousel className={`mt-12 max-w-96 px-1 sm:max-w-xl lg:max-w-full ${cn}`}>
+      <CarouselContent className="relative px-0">
         {products?.map((prod) => (
-          <CarouselItem key={prod._id} className={`sm:basis-1/2 sm:p-2 md:basis-1/3 ${products.length > 3 && "lg:basis-1/4"}`}>
-            {/* <div className="p-1"> */}
+          <CarouselItem key={prod._id} className={`basis-full sm:basis-1/2 lg:basis-1/4 ${products.length > 3 ? "lg:basis-1/4" : "lg:basis-1/3"}`}>
             <Card className="mx-auto max-w-fit border-none">
               <CardContent className="flex aspect-square items-center justify-center">
                 <Product carousel product={prod} />
               </CardContent>
             </Card>
-            {/* </div> */}
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="absolute left-0 z-10 rounded-full bg-black bg-opacity-50 p-2 text-white" />
-      <CarouselNext className="absolute right-0 z-10 rounded-full bg-black bg-opacity-50 p-2 text-white" />
+      <CarouselPrevious
+        {...(products && products?.length < 3 ? { disabled: true } : {})}
+        disabled
+        className="absolute left-0 z-10 rounded-full bg-black bg-opacity-50 p-2 text-white"
+      />
+      <CarouselNext
+        {...(products && products?.length < 3 ? { disabled: true } : {})}
+        className="absolute right-0 z-10 rounded-full bg-black bg-opacity-50 p-2 text-white"
+      />
     </Carousel>
   );
 }
+
+// {...(!isHoveredOn || !isFocused ? { asChild: true } : {})}
