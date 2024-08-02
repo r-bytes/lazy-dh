58d511eccedf111418dd3d66af837b7c3fb38e91
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-02 20:01:37 +0200

    fixes

--- src/app/(pages)/admin/(pages)/bestellingen/page.tsx
+++ src/app/(pages)/admin/(pages)/bestellingen/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import MaxWidthWrapper from "@/components/ui/max-width-wrapper";
 import OrderManagement from "./order-management";
-import withAuth from "@/hoc/withAuth";
 import { useEffect, useState } from "react";
 import { Order } from "@/lib/types/order";
 import { fetchAllOrders } from "@/lib/db/data";
