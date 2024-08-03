bba1f3917cd9d2acaa126cb33fad2b3c360787b2
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-03 18:32:23 +0200

    fixes

--- src/app/(pages)/account/registreer/bevestig-email/page.tsx
+++ src/app/(pages)/account/registreer/bevestig-email/page.tsx
@@ -1,39 +1,51 @@
+"use client";
+
 import { verifyEmail } from "@/actions/users/user.actions";
 import { db } from "@/lib/db";
 import { users } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface VerifyEmailPageProps {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
-const VerifyEmailPage = async ({ searchParams }: VerifyEmailPageProps) => {
-  if (searchParams.token) {
-    const user = await db.query.users.findFirst({
-      where: eq(users.emailVerificationToken, searchParams.token as string),
-    });
+const VerifyEmailPage = ({ searchParams }: VerifyEmailPageProps) => {
+  const [message, setMessage] = useState("Loading...");
+  const router = useRouter();
 
-    await verifyEmail(searchParams.token as string);
+  useEffect(() => {
+    const verifyAndRedirect = async () => {
+      if (searchParams.token) {
+        const user = await db.query.users.findFirst({
+          where: eq(users.emailVerificationToken, searchParams.token as string),
+        });
 
-    if (!user) {
-      return <div className="flex items-center justify-center">Invalid token</div>;
-    }
+        if (!user) {
+          setMessage("Ongeldige token");
+          return;
+        }
 
-    return (
-      <div className="flex items-center justify-center">
-        <h1>
-          Email bevestigd voor <b>{user.email}</b>!
-        </h1>
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex items-center justify-center">
-        <h1>Verify Email</h1>
-        No email verification token found. Check your email.
-      </div>
-    );
-  }
+        await verifyEmail(searchParams.token as string);
+        setMessage(`Email bevestigd voor ${user.email}!`);
+
+        setTimeout(() => {
+          router.replace("/auth");
+        }, 5000); // Redirect after 5 seconds
+      } else {
+        setMessage("Geen email verificatie token gevonden. Check je email.");
+      }
+    };
+
+    verifyAndRedirect();
+  }, [searchParams.token, router]);
+
+  return (
+    <div className="flex items-center justify-center">
+      <h1>{message}</h1>
+    </div>
+  );
 };
 
 export default VerifyEmailPage;
--- src/app/(pages)/categorieen/page.tsx
+++ src/app/(pages)/categorieen/page.tsx
@@ -1,11 +1,18 @@
 import { CategoryCard } from "@/components/ui/category/category-card";
 import MaxWidthWrapper from "@/components/ui/max-width-wrapper";
+import { fetchCategories } from "@/lib/sanity/fetchCategories";
+import { fetchProducts } from "@/lib/sanity/fetchProducts";
+import { Category } from "@/lib/types/category";
+import Product from "@/lib/types/product";
 
 export default async function Page() {
+  const productList: Product[] = await fetchProducts("");
+  const categoryList: Category[] = await fetchCategories();
+
   return (
     <div className="mx-auto flex flex-col items-center justify-between bg-background lg:max-w-7xl lg:p-24">
       <MaxWidthWrapper>
-        <CategoryCard />
+        <CategoryCard categories={categoryList} products={productList} />
       </MaxWidthWrapper>
     </div>
   );
