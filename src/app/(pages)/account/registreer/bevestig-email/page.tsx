d093eb1afa3d93fd2b48b1833794b84625adf95d
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-03 21:39:16 +0200

    fixes

--- src/app/(pages)/account/registreer/bevestig-email/page.tsx
+++ src/app/(pages)/account/registreer/bevestig-email/page.tsx
@@ -1,51 +1,39 @@
-"use client";
-
 import { verifyEmail } from "@/actions/users/user.actions";
 import { db } from "@/lib/db";
 import { users } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 
 interface VerifyEmailPageProps {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
-const VerifyEmailPage = ({ searchParams }: VerifyEmailPageProps) => {
-  const [message, setMessage] = useState("Loading...");
-  const router = useRouter();
+const VerifyEmailPage = async ({ searchParams }: VerifyEmailPageProps) => {
+  if (searchParams.token) {
+    const user = await db.query.users.findFirst({
+      where: eq(users.emailVerificationToken, searchParams.token as string),
+    });
 
-  useEffect(() => {
-    const verifyAndRedirect = async () => {
-      if (searchParams.token) {
-        const user = await db.query.users.findFirst({
-          where: eq(users.emailVerificationToken, searchParams.token as string),
-        });
+    await verifyEmail(searchParams.token as string);
 
-        if (!user) {
-          setMessage("Ongeldige token");
-          return;
-        }
+    if (!user) {
+      return <div className="flex items-center justify-center">Invalid token</div>;
+    }
 
-        await verifyEmail(searchParams.token as string);
-        setMessage(`Email bevestigd voor ${user.email}!`);
-
-        setTimeout(() => {
-          router.replace("/auth");
-        }, 5000); // Redirect after 5 seconds
-      } else {
-        setMessage("Geen email verificatie token gevonden. Check je email.");
-      }
-    };
-
-    verifyAndRedirect();
-  }, [searchParams.token, router]);
-
-  return (
-    <div className="flex items-center justify-center">
-      <h1>{message}</h1>
-    </div>
-  );
+    return (
+      <div className="flex items-center justify-center">
+        <h1>
+          Email bevestigd voor <b>{user.email}</b>!
+        </h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex items-center justify-center">
+        <h1>V Bevestig email l</h1>
+        Geen token gevonden
+      </div>
+    );
+  }
 };
 
 export default VerifyEmailPage;
