41b7b442ce34f972516afd86862cf091dd2ebb9d
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-05 22:19:08 +0200

    cors

--- src/lib/sanity/fetchCategories.ts
+++ src/lib/sanity/fetchCategories.ts
@@ -2,6 +2,8 @@ import { Category } from "../types/category";
 
 export const fetchCategories = async () => {
   const response: Response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getCategories`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
     cache: "force-cache", // SSG
     // cache: "no-store", // SSR
     // next: {
