7f79d99180e4ad76867f91981df116dec65f9821
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-05 23:09:59 +0200

    force dynamic

--- src/app/api/favorites/status/route.ts
+++ src/app/api/favorites/status/route.ts
@@ -1,6 +1,9 @@
 import { checkFavoriteStatus } from "@/lib/db/data";
 import { NextRequest, NextResponse } from "next/server";
 
+// Explicitly state that this is a dynamic route
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
