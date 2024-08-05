3fa1e76d11c96bae8e7cc0e1ebf92170179a7831
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-05 22:59:12 +0200

    cors errors

--- src/app/api/getCategories/route.ts
+++ src/app/api/getCategories/route.ts
@@ -7,27 +7,24 @@ type Data = {
   categories: Category[];
 };
 
+const query: string = groq`
+  *[_type == "category"] | order(order asc)
+`;
+
 export const GET = async (req: NextRequest): Promise<NextResponse<Data>> => {
   const categories: Category[] = await sanityClient.fetch(query);
-  return NextResponse.json({ categories });
+
+  const response = NextResponse.json({ categories });
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  return response;
 };
 
-  const query: string = groq`
-    *[_type == "category"] | order(order asc)
-  `;
-
-export const middleware = async (req: NextRequest) => {
-  if (req.method === "OPTIONS") {
-    const headers = new Headers();
-    headers.append("Access-Control-Allow-Origin", "*");
-    headers.append("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    headers.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    return new Response(null, { status: 204, headers });
-  }
-
-  const response = await GET(req);
-  response.headers.append("Access-Control-Allow-Origin", "*");
-  response.headers.append("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  response.headers.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  return response;
+export const OPTIONS = async (): Promise<NextResponse> => {
+  const headers = new Headers();
+  headers.append("Access-Control-Allow-Origin", "*");
+  headers.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  headers.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  return new NextResponse(null, { status: 204, headers });
 };
