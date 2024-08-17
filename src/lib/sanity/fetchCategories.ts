128ec279d6c16303ad299397d04e64fcbb7ddf72
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-17 15:55:59 +0200

    enable cache

--- src/lib/sanity/fetchCategories.ts
+++ src/lib/sanity/fetchCategories.ts
@@ -4,8 +4,8 @@ export const fetchCategories = async () => {
   const response: Response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getCategories`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
-    // cache: "force-cache", // SSG
-    cache: "no-store", // SSR
+    cache: "force-cache", // SSG
+    // cache: "no-store", // SSR
     // next: {
     //     revalidate: 20, // ISR
     // },
