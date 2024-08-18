f3589b8efce687b7661015e9b5bac8f80bc4cd77
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-08-18 14:35:16 +0200

    fix: new user requests should be highlighted

--- src/app/(pages)/admin/(pages)/accounts/user-management.tsx
+++ src/app/(pages)/admin/(pages)/accounts/user-management.tsx
@@ -127,7 +127,7 @@ const UserManagement = ({ allUsers, userId }: UserManagementProps) => {
           {users
             .filter((user) => !showApproved || !user.admin_approved || !authorizedEmails.includes(user.email))
             .map((user) => (
-              <TableRow key={user.id} className={user.id === userId ? "bg-primary/20" : ""}>
+              <TableRow key={user.id} className={!editedUsers[user.id] ? "bg-primary/20" : ""}>
                 <TableCell>{user.name}</TableCell>
                 <TableCell>{user.email}</TableCell>
                 <TableCell>{user.address}</TableCell>
