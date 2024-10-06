9e17ae92a2842b01679b836cf817e0fee7f3f968
raymond.vanvelse@netprogroup.com <raymond.vanvelse@netprogroup.com>
2024-10-06 20:13:37 +0200

    fix: color

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -16,7 +16,7 @@ const Footer: React.FC<FooterProps> = ({ year = new Date().getFullYear() }) => {
           quality={100}
           priority
         />
-        <div className="bg-black bg-opacity-70 backdrop-blur-3xl h-48 flex items-center flex-col justify-center">
+        <div className="bg-black bg-opacity-70 backdrop-blur-3xl h-48 flex items-center flex-col justify-center text-white/70">
           <div className="flex gap-4 max-w-5xl w-full justify-evenly flex-wrap">
             <a href="/algemene-voorwaarden" className="text-sm hover:text-primary-hover transition-colors duration-200 hover:underline hover:scale-105 transform">
               Algemene voorwaarden
