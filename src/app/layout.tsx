4b37ea8a215e6d93fe5a0c00779cf804d2643cc8
rvelse <rvv@duck.com>
2025-12-01 21:51:12 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,19 +33,19 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico?v=2", sizes: "any" },
-      { url: "/favicon-16x16.png?v=2", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png?v=2", sizes: "32x32", type: "image/png" },
-      { url: "/android-chrome-192x192.png?v=2", sizes: "192x192", type: "image/png" },
-      { url: "/android-chrome-512x512.png?v=2", sizes: "512x512", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
-      { url: "/apple-touch-icon.png?v=2", sizes: "180x180", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
     other: [
       {
         rel: "manifest",
-        url: "/site.webmanifest?v=2",
+        url: "/site.webmanifest",
       },
     ],
   },
